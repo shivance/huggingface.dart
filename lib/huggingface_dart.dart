f4bf1d4ab4db1604477b5e0ddc09c2b036be883b
Anshuman Mishra <anshuman@flip.space>
2024-01-07 15:48:49 +0530

    add working prototype for translation api

--- lib/huggingface_dart.dart
+++ lib/huggingface_dart.dart
@@ -1,53 +1,97 @@
 library huggingface_dart;
+
 import 'dart:convert';
+import 'dart:core';
 
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import 'package:huggingface_dart/models/options.dart';
+import 'package:huggingface_dart/models/translation.dart';
 
+class HfInference {
+  final String? accessToken;
+  final String apiUrl = "https://api-inference.huggingface.co/models";
 
+  HfInference(this.accessToken);
 
-class InferenceAPI {
-  final String apiUrl;
-  final String apiToken;
-
-  InferenceAPI(this.apiUrl, this.apiToken);
-
-  Future<List<dynamic>> query(String payload) async {
+  Future<Response> queryHF(
+      Map<String, dynamic> queryParams, String model) async {
     final Map<String, String> headers = {
-      "Authorization": "Bearer $apiToken"
+      "Authorization": "Bearer $accessToken",
+      "Content-Type": "application/json",
     };
-    final http.Response response = await http.post(
-      Uri.parse(apiUrl),
-      headers: headers,
-      body: jsonEncode(({"inputs": payload})),
-    );
 
-    print(response);
-    if (response.statusCode == 200) {
-      return json.decode(response.body);
-    } else {
-      throw Exception("Failed to query the API");
-    }
-    }
+    String endpointUrl = "$apiUrl/$model";
+
+    final Response response = await post(Uri.parse(endpointUrl),
+        headers: headers, body: json.encode(queryParams));
+
+    return response;
   }
 
-class HfInference{
-  final String apiToken;
+  Future<Response> queryHFWithRetries(
+      Map<String, dynamic> queryParams, String model,
+      {int maxRetries = 5}) async {
+    int retryCount = 0;
+    Exception? lastError;
 
-  HfInference(this.apiToken);
+    while (retryCount < maxRetries) {
+      try {
+        Response response = await queryHF(queryParams, model);
 
-}
+        if (response.statusCode == 200) {
+          return response;
+        } else {
+          lastError ??=
+              Exception("Received non-200 status code: ${response.statusCode}");
+          print(lastError);
+        }
+      } catch (e) {
+        lastError ??= e as Exception;
+        print("Error: $e");
+      }
 
+      print("Retrying... (${retryCount + 1}/$maxRetries)");
+      retryCount++;
 
-void main() async{
-  final String apiUrl = "https://api-inference.huggingface.co/models/gpt2";
-  final String apiToken = "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";
+      // You can add a delay between retries if needed.
+      await Future.delayed(Duration(seconds: 1));
+    }
 
-  final InferenceAPI inferenceAPI = InferenceAPI(apiUrl, apiToken );
+    // If all retries fail, throw the last encountered error.
+    throw lastError ??
+        Exception("Failed to query the API after $maxRetries retries");
+  }
 
-  try {
-    List<dynamic> data = await inferenceAPI.query("Can you please let us know more details about your");
-    print(data);
-  } catch (e) {
-    print("Error: $e");
+  Future<TranslationOutput> translate(
+      {required TranslationArgs args,
+      required String model,
+      OptionalArgs? optionalArgs}) async {
+    Map<String, dynamic> queryParams = args.toJson();
+
+    if (optionalArgs != null) {
+      Map<String, dynamic> optionalJson = optionalArgs.toJson();
+      queryParams.addAll(optionalJson);
+    }
+
+    final response = await queryHFWithRetries(
+      queryParams,
+      model,
+    );
+
+    print("Response = ${response.body}");
+
+    if (response.statusCode == 200) {
+      dynamic decodedResponse = json.decode(response.body);
+
+      if (decodedResponse is List) {
+        return TranslationOutput.fromJson({
+          'translation_text': decodedResponse,
+        });
+      } else {
+        throw Exception("Unexpected response format");
+      }
+    } else {
+      throw Exception("Failed to query the translation API");
+    }
   }
 }
